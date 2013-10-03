111cae650b61d336e23bfff39ceff488f8208a1c
Tim Cameron Ryan <id@timryan.org>
2013-10-03 00:11:22 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var rem = require('rem');
+var scrapi = require('scrapi');
+
+var all = {};
+rem.stream('http://olindining.com/CampusCenterDiningWeek1_005.htm').get().pipe(scrapi.parser({
+  'breakfast': {
+    $query: '.brk',
+    $each: '(text)'
+  },
+  'lunch': {
+    $query: '.lun',
+    $each: '(text)'
+  },
+  'dinner': {
+    $query: '.din',
+    $each: '(text)'
+  }
+}, function (res) {
+  function parse (datums) {
+    var brk = [];
+    var day = {};
+    var cur = [];
+    datums.forEach(function (b) {
+      b = b.substr(1);
+      if (b == 'REAKFAST' || b == 'UNCH' || b == 'INNER') {
+        day = [];
+        brk.push(day);
+      } else {
+        console.log(JSON.stringify(b));
+        if (!b.match(/^[\r\n]/)) {
+          cur = [];
+          day[b.match(/^[^\r]+/)[0]] = cur;
+          b = b.replace(/^[^\r]+/, '');
+        }
+        cur.push(b.replace(/^\s+|\s+$/g, ''));
+      }
+    });
+    return brk;
+  }
+
+  all = {
+    breakfast: parse(res.breakfast),
+    lunch: parse(res.lunch),
+    dinner: parse(res.dinner),
+  };
+}))
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.json(all);
+})
+
+app.listen(3000);
