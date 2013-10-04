ea52e752ab9401ee62c334394f5468b9a26ac905
Tim Cameron Ryan <id@timryan.org>
2013-10-03 22:40:51 -0400

    Awesome

--- app.js
+++ app.js
@@ -4,117 +4,150 @@ var skim = require('skim');
 
 var daynames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-var meals = [];
-var nutrition = {};
-rem.stream('http://olindining.com/CampusCenterDiningWeek1_005.htm').get().pipe(skim({
-  'breakfast': {
-    $query: '.brk',
-    $each: '(text)'
-  },
-  'lunch': {
-    $query: '.lun',
-    $each: '(text)'
-  },
-  'dinner': {
-    $query: '.din',
-    $each: '(text)'
-  },
-  'scripts': {
-    $query: 'script',
-    $each: '(text)'
-  }
-}, function (res) {
-  if (!res.scripts[1]) {
-    console.error('No data :(');
+function getMenuStream (next) {
+  var dif, d = new Date(); // Today's date 
+  dif = (d.getDay() + 6) % 7; // Number of days to subtract 
+  d = new Date(d - dif * 24*60*60*1000); // Do the subtraction 
+  var dstr = (d.getMonth() + 1) + '/' + d.getDate();
+
+  rem.stream('http://olindining.com/').get().pipe(skim({
+    'links': {
+      $query: '#front',
+      $value: '(html)'
+    }
+  }, function (a) {
+    next(a.links.match(new RegExp('href=\"(.*?)\">.*?' + dstr))[1]);
+    // var L = null;
+    // console.log(a.links);
+    // a.links.forEach(function (l) {
+    //   // console.log(l.str, dstr)
+    //   if (l.str.indexOf(dstr) > -1 && l.str.indexOf('Week of') > -1) {
+    //     L = l;
+    //   }
+    // });
+    // next(L.href);
+  }));
+}
+
+getMenuStream(function (href) {
+  if (!href) {
+    console.error('BAD NEWS', href);
     return;
   }
 
-  var nut_keys = ['serv_size',
-  'calories', 'fat_calories',
-  'fat', 'percent_fat_dv',
-  'satfat', 'percent_satfat',
-  'trans_fat',
-  'cholesterol', 'percent_cholesterol',
-  'sodium', 'percent_sodium',
-  'carbo', 'percent_carbo',
-  'dfib', 'percent_dfib',
-  'sugars', 'protein',
-  'a', 'cp', 'up', 'ip',
-  'name', 'description', 'allergen',
-  'percent_vit_a_dv',
-  'percent_vit_c_dv',
-  'percent_calcium_dv',
-  'percent_iron_dv',
-  '_'];
+  var meals = [];
+  var nutrition = {};
+  rem.stream('http://olindining.com/' + href).get().pipe(skim({
+    'breakfast': {
+      $query: '.brk',
+      $each: '(text)'
+    },
+    'lunch': {
+      $query: '.lun',
+      $each: '(text)'
+    },
+    'dinner': {
+      $query: '.din',
+      $each: '(text)'
+    },
+    'scripts': {
+      $query: 'script',
+      $each: '(text)'
+    }
+  }, function (res) {
+    if (!res.scripts[1]) {
+      console.error('No data :(');
+      return;
+    }
 
-  var nutrition_bad = eval(res.scripts[3].replace('<!--', '//') + '; aData');
-  nutrition = {};
-  Object.keys(nutrition_bad).forEach(function (key) {
-    var a = nutrition[nutrition_bad[key][22]] = {};
-    nutrition_bad[key].forEach(function (val, i) {
-      a[nut_keys[i]] = val;
+    var nut_keys = ['serv_size',
+    'calories', 'fat_calories',
+    'fat', 'percent_fat_dv',
+    'satfat', 'percent_satfat',
+    'trans_fat',
+    'cholesterol', 'percent_cholesterol',
+    'sodium', 'percent_sodium',
+    'carbo', 'percent_carbo',
+    'dfib', 'percent_dfib',
+    'sugars', 'protein',
+    'a', 'cp', 'up', 'ip',
+    'name', 'description', 'allergen',
+    'percent_vit_a_dv',
+    'percent_vit_c_dv',
+    'percent_calcium_dv',
+    'percent_iron_dv',
+    '_'];
+
+    var nutrition_bad = eval(res.scripts[3].replace('<!--', '//') + '; aData');
+    nutrition = {};
+    Object.keys(nutrition_bad).forEach(function (key) {
+      var a = nutrition[nutrition_bad[key][22]] = {};
+      nutrition_bad[key].forEach(function (val, i) {
+        a[nut_keys[i]] = val;
+      })
     })
-  })
 
-  try {
-    function parse (datums) {
-      var brk = [];
-      var day = {};
-      var cur = [];
-      datums.forEach(function (b) {
-        b = b.substr(1);
-        if (b == 'REAKFAST' || b == 'UNCH' || b == 'INNER') {
-          day = {};
-          brk.push(day);
-        } else {
-          if (!b.match(/^[\r\n]/)) {
-            cur = [];
-            day[String(b.match(/^[^\r]+/)[0])] = cur;
-            b = b.replace(/^[^\r]+/, '');
+    try {
+      function parse (datums) {
+        var brk = [];
+        var day = {};
+        var cur = [];
+        datums.forEach(function (b) {
+          b = b.substr(1);
+          if (b == 'REAKFAST' || b == 'UNCH' || b == 'INNER') {
+            day = {};
+            brk.push(day);
+          } else {
+            if (!b.match(/^[\r\n]/)) {
+              cur = [];
+              day[String(b.match(/^[^\r]+/)[0])] = cur;
+              b = b.replace(/^[^\r]+/, '');
+            }
+
+            var name = String(b.replace(/^\s+|\s+$/g, ''));
+            cur.push({
+              name: name,
+              nutrition: nutrition[name]
+            });
           }
-
-          var name = String(b.replace(/^\s+|\s+$/g, ''));
-          cur.push({
-            name: name,
-            nutrition: nutrition[name]
-          });
-        }
-      });
-      return brk;
-    }
-
-    // console.log(meals);
-
-    var breakfast = parse(res.breakfast);
-    var lunch = parse(res.lunch);
-    var dinner = parse(res.dinner);
-
-    for (var i = 0; i < breakfast.length; i++) {
-      meals.push({
-        dayname: daynames[i],
-        breakfast: breakfast[i],
-        lunch: lunch[i],
-        dinner: dinner[i]
-      });
-    }
-  } catch (e) {
-    console.log('ERROR:', e);
-  }
-
-  console.log('SUCCESSFUL PARSING');
-  nutrition = JSON.parse(JSON.stringify(meals));
-  meals.forEach(function (day) {
-    Object.keys(day).forEach(function (ok) {
-      if (typeof day[ok] == 'object') {
-        Object.keys(day[ok]).forEach(function (section) {
-          day[ok][section].forEach(function (meal) {
-            delete meal.nutrition;
-          })
-        })
+        });
+        return brk;
       }
+
+      // console.log(meals);
+
+      var breakfast = parse(res.breakfast);
+      var lunch = parse(res.lunch);
+      var dinner = parse(res.dinner);
+
+      for (var i = 0; i < breakfast.length; i++) {
+        meals.push({
+          dayname: daynames[i],
+          breakfast: breakfast[i],
+          lunch: lunch[i],
+          dinner: dinner[i]
+        });
+      }
+    } catch (e) {
+      console.log('ERROR:', e);
+    }
+
+    console.log('SUCCESSFUL PARSING');
+    nutrition = JSON.parse(JSON.stringify(meals));
+    meals.forEach(function (day) {
+      Object.keys(day).forEach(function (ok) {
+        if (typeof day[ok] == 'object') {
+          Object.keys(day[ok]).forEach(function (section) {
+            day[ok][section].forEach(function (meal) {
+              delete meal.nutrition;
+            })
+          })
+        }
+      })
     })
-  })
-}))
+  }))
+});
+
 
 var app = express();
 
